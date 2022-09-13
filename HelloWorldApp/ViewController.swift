80163073ee756325a8862fb20b39929567e535be
Amirkhan <aak82183@gmail.com>
2022-09-13 13:44:22 +0300

    Traffic light

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -2,33 +2,58 @@
 //  ViewController.swift
 //  HelloWorldApp
 //
-//  Created by Amirkhan Akaev on 02.09.2022.
+//  Created by Amirkhan Akaev on 10.09.2022.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TrafficLight {
+    case red, yellow, green
+}
 
-    @IBOutlet var helloWorldLabel: UILabel!
-    @IBOutlet var showTextButton: UIButton!
-   
+final class ViewController: UIViewController {
+    
+    @IBOutlet private var redLight: UIView!
+    @IBOutlet private var yellowLight: UIView!
+    @IBOutlet private var greenLight: UIView!
+    @IBOutlet private var startButton: UIButton!
+    
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.2
+    private var currentLight: TrafficLight = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
     }
-
-    @IBAction func showButtonPressed() {
-        helloWorldLabel.isHidden.toggle()
-        helloWorldLabel.isHidden
-        ? showTextButton.setTitle("Show Text", for: .normal)
-        : showTextButton.setTitle("Hide Text", for: .normal)
+    
+    private func setupUI() {
+        startButton.layer.cornerRadius = 10
+        startButton.setTitle("Start", for: .normal)
+        
+        redLight.layer.cornerRadius = redLight.bounds.height / 2
+        yellowLight.layer.cornerRadius = yellowLight.bounds.height / 2
+        greenLight.layer.cornerRadius = greenLight.bounds.height / 2
     }
     
-    func setupUI() {
-        helloWorldLabel.isHidden = true
-        showTextButton.layer.cornerRadius = 10
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case.red:
+            redLight.alpha = lightOn
+            greenLight.alpha = lightOff
+            currentLight = .yellow
+        case.yellow:
+            yellowLight.alpha = lightOn
+            redLight.alpha = lightOff
+            currentLight = .green
+        case.green:
+            greenLight.alpha = lightOn
+            yellowLight.alpha = lightOff
+            currentLight = .red
+        }
     }
     
 }
-
